d0aca59c5483914e309a5d864529cd1ce548fd7c
Kenia Rodriguez <keniaaqrodriguez@gmail.com>
2021-06-12 17:24:46 -0400

    setup complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,55 @@
+import { useEffect, useState } from "react";
+import { Switch, Route } from "react-router-dom";
+import axios from "axios";
+import NavBar from "./Components/NavBar";
+import Home from "./Components/Home";
+import Logs from "./Components/Logs";
+import { apiURL } from "./util/apiURL";
+
+// further down..., but still above `App` component
+const API = apiURL();
+
 function App() {
-  return <div>Hello World</div>;
+  const { logs, setLogs } = useState;
+
+  const fetchLogs = async () => {
+    axios
+      .get(`${API}/logs`)
+      .then(
+        (response) => setLogs(response.data),
+        (error) => console.log("get", error)
+      )
+      .catch((c) => console.warn("catch", c));
+  };
+
+  useEffect(() => {
+    fetchLogs();
+  }, []);
+
+  return (
+    <div>
+      <NavBar />
+      <main>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/logs">
+            <Logs logs={logs} />
+          </Route>
+          {/* <Route path="/logs/new">
+            <New addBookmark={addBookmark} />
+          </Route>
+          <Route exact path="/logs/:index">
+            <Show bookmarks={bookmarks} deleteBookmark={deleteBookmark} />
+          </Route>
+          <Route path="*">
+            <FourOFour />
+          </Route> */}
+        </Switch>
+      </main>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+
+export default function NavBar() {
+    return (
+        <nav>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/logs">Logs</NavLink>
+            <NavLink to="/logs/new">New Logs</NavLink>
+            <NavLink to="/logs/:index">Logs at Index</NavLink>
+        </nav>
+    )
+}
