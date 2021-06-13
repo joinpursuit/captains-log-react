8f1512f32aaa3690c46fc78a8537557be9006830
Demi <oluwademiladejobi@pursuit.org>
2021-06-13 15:47:15 -0400

    set up NavBar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from "axios";
+
+import NavBar from "./Components/NavBar";
+
+import { apiURL } from "./util/apiURL";
+
+const API = apiURL()
+
 function App() {
-  return <div>Hello World</div>;
+const [logs, setLogs] = useState([])
+
+  const fetchLogs = async() =>{
+    try{
+      let res = await axios.get(`${API}/logs`)
+      setLogs(res.data)
+    }catch(err){
+      console.log(err)
+    }
+  }
+useEffect(()=>{
+  fetchLogs()
+})
+
+  return (
+    <div>
+      <Router>
+      <NavBar/>
+      <h1>Welcome to the Captain's Log</h1>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
