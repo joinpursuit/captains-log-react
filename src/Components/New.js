b4e85229b9b4f69b47788904e25e4fe7125c9d17
Miguel Garcia <miguelgarcia@pursuit.org>
2021-06-14 20:42:59 -0400

    form started

--- src/Components/New.js
+++ src/Components/New.js
@@ -1,20 +1,40 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
 const New = () => {
-  const form = () => {
+
+    const [log, setLog] = useState({
+      captainName: "",
+      title: "",
+      post: "",
+      mistakesWereMadeToday: false,
+      daysSinceLastCrisis: 0
+    });
+
+    const handleChange = () => {
+      
+    }
+    
     return (
       <div>
         <h1>Add Log</h1>
         <form>
-          <input type="text"/>
-          <input type="text"> Title: </input>
-          <input type="text"> Post: </input>
-          <input type="text"> Mistakes Were Made Today: </input>
-          <input type="number"> Days Since Last Crisis: </input>
+          <label htmlFor="captainName">Captain's Name</label>
+          <input type="text" id="captainName" value={captainName} />
+          <label htmlFor="title">Title:</label>
+          <input type="text" id="title" />
+          <label htmlFor="post">Post:</label>
+          <input type="text" id="post" />
+          <label htmlFor="mistakes">Mistakes Were MAde Today:</label>
+          <input type="text" id="mistakes" />
+          <label htmlFor="lastCrisis">Days Since Last Crisis:</label>
+          <input type="number" id="lastCrisis" />
         </form>
       </div>
     );
-  };
+  
 
-  return form ;
+
 };
 
 export default New;
