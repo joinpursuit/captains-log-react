4f552b157513c24d33a34882beb1941f98c2b2a7
Lakisha J <lakishajohnson@pursuit.org>
2022-01-13 18:55:22 -0500

    1 functional route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import NavBar from "./components/Navbar";
+import NavBar from "./Components/Navbar";
+import Home from "./Pages/Home";
+import Index from "./Pages/Index";
 
 function App() {
   return (
     <div className="App">
+        {/* <h1>IT WORKS</h1> */}
       <Router>
         <NavBar />
+
+          <Routes>
+          <Route path="/" element={<Home />} />
+            <Route path="/logs" element={<Index />} />
+          </Routes>
+      
       </Router>
     </div>
   );
