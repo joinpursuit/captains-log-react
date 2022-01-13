6d27cfcd736025c562a8816db822385df717b6c0
Rose Durandisse <rosedurandisse@pursuit.org>
2022-01-13 17:37:38 -0500

    Updated App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+//Pages
+import Logs from './Pages/Log';
+
+//Components
+import NavBar from "./Components/NavBar";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Router>
+        <NavBar />
+        <main>
+        <Routes>
+            <Route path="/logs" element={<Logs />} />
+        </Routes>
+        </main>
+      </Router>
+    </div>
+  )
 }
+
 
 export default App;
