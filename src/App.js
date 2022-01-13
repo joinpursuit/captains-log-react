0a55725ade32bf9ed5e7c14d90ebd81ef09d509b
stephanie-lucano <stephaniemarielucano@pursuit.org>
2022-01-13 16:14:45 -0500

    Converted functional component to class component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react"
 
-function App() {
-  return <div>Hello World</div>;
+class App extends React.Component {
+  render() {
+    return (
+      <div>Hello World</div>
+      )
+    }
 }
 
 export default App;
