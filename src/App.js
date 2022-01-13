5b34b4d305f45a7611044cc8e77e6ef2ed6b724c
stephanie-lucano <stephaniemarielucano@pursuit.org>
2022-01-13 16:58:36 -0500

    Converted class to functional component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react"
 import Nav from "./Components/Nav"
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Nav />
-      </div>
-      )
-    }
+function App() {
+  return (
+    <div>
+      <Nav />
+    </div>
+    )  
 }
 
 export default App;
