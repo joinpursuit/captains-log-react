cdd8b30e5415f2f9730c016d393e0f3c78e17ef4
stephanie-lucano <stephaniemarielucano@pursuit.org>
2022-01-13 17:39:32 -0500

    Replace React import to Routes and Route import and add Home Routes to the return with a boiler plate for other Route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from "react"
+import { Routes, Route } from "react-router-dom"
 import Nav from "./Components/Nav"
+import Home from "./Components/Home"
 
 function App() {
   return (
     <div>
       <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="" element={} /> */}
+      </Routes>
     </div>
     )  
 }
