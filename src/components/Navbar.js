a7ec92d7528765dc326c7ca2db5fe21f61405ca9
Lakisha J <lakishajohnson@pursuit.org>
2022-01-13 18:51:46 -0500

    /logs route functional

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 class NavBar extends React.Component {
     render() {
   return (
-    <nav>
-      <h1>
-        <Link to="/">Captains Log</Link>
-        </h1>
-        <h1>
-        <Link to="/logs/new">New Bookmark</Link>
-      </h1>
+    <nav className="Navbar">
+      <h3>
+        <Link to="/logs">Captains Log</Link>
+        </h3>
+        <h3>
+        <Link to="/logs/new">New Log</Link>
+      </h3>
       
     </nav>
   );
