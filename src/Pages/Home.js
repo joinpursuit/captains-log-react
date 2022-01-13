396f8d653bb164388632b77f6c5d558fcff583bf
Lakisha J <lakishajohnson@pursuit.org>
2022-01-13 18:50:09 -0500

    Component renders a list of logs

---
+++ src/Pages/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+      <div className="Home">
+        <h1>Welcome</h1>
+        <h2>To the greatest Captains Log app!</h2>
+      </div>
+    );
+  }
+  
+  export default Home;
