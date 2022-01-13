fb4ddae155ec363452c2b43c2ff2d6f465e2748a
BennyZ3 <bennyz3@hotmail.com>
2022-01-13 18:26:28 -0500

    added paths, react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import LogIndex from "./components/LogIndex";
+import Logs from "./components/logs";
+import Nav from "./components/Nav";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div className="App">
+      {/* Nav */}
+      <Nav />
+      {/* main */}
+      <main>
+        {/* routes */}
+        <Routes>
+          {/* '/' home */}
+          <Route exact path="/" element={<Home />} />
+          {/* '/logs' */}
+          <Route path="/logs" element={<Logs />} />
+          {/* '/logs/:index */}
+          <Route path="/logs/:index" element={<LogIndex />} />
+          {/* '/logs/new' */}
+          {/* '/logs/edit */}
+        </Routes>
+        {/* Hello World */}
+      </main>
+    </div>
+  );
 }
 
 export default App;
