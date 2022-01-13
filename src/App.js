ca33a5295eaefb20850c67b574b8e7f4c2c29ac6
Triane Peart <TrianePeart@gmail.com>
2022-01-13 13:18:51 -0500

    Added Router to App

--- src/App.js
+++ src/App.js
@@ -4,14 +4,45 @@ import axios from "axios";
 
 import Nav from "./components/Nav"
 import Home from "./components/Home"
-import Log from "./components/Log"
 import Logs from "./components/Logs"
 import LogDetails from "./components/LogDetails"
-import NewLogs from "././components/NewLogs"
+import NewLogs from "./components/NewLogs"
+import EditLogs from "../components/EditLogs";
 
 const App = () => {
-  
+
+    
+    return (
+        <div className="App">
+            <Router>
+                <Nav/>
+                <Switch>
+                    <Route exact path="/">
+                        <Home/>
+                    </Route>
+
+                    <Route exact path="/logs">
+                        <Logs/>
+                    </Route>
+                    
+                    <Route exact path="/logs/:index">
+                        <LogDetails/>
+                    </Route>
+
+                    <Route exact path="/logs/:new">
+                        <NewLogs/>
+                    </Route>
+
+                    <Route exact path="/logs/:edit">
+                        <EditLogs/>
+                    </Route>
+
+                    <Route path="*"></Route>
+                </Switch>
+            </Router>
+        </div>
+    )
 }
 
-
+//That star path night not be needed just go back and delete later if anything. 
 export default App;
