4306286874b731c4f780129ca940174972f13f69
Rose Durandisse <rosedurandisse@pursuit.org>
2022-01-13 17:34:26 -0500

    Updated Logs and NavBar

---
+++ src/Components/Logs.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import { useEffect, useState } from "react";
+
+function Log() {
+    const [logs,setLogs] = useState([]);
+    const URL = process.env.REACT_APP_API_URL;
+
+    useEffect(()=> {
+        axios.get(`${URL}/logs`).then(
+        (response)=>{
+        setLogs(response.data)   
+        })
+    }, [URL])
+
+
+const logFile = logs.map((eachLog, index) => {
+    return <li key={index} index={index}>
+    <p>Captain Name:{eachLog.captainName}</p>
+    <p>Post Title:{eachLog.title}</p> 
+    <p>Post:{eachLog.post}</p></li>;
+})
+
+return (
+    <p>
+<ul>     
+{logFile}
+</ul>
+    </p>
+)
+}
+
+
+export default Log;
