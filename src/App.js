b4fbdbffc0420c675b34d73e3988396c6c252fa8
Rohan-Sterling <rohansterling@pursuit.org>
2022-01-14 13:53:24 -0500

    imported NavBar and Logs components, imported routes and route from react-router-dom, return routes  in app function component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react"
+import NavBar from "./Components/Common/NavBar"
+import Logs from "./Components/Pages/Routes/Logs"
+import {Routes, Route} from "react-router-dom"
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div className="wrap">
+      <NavBar/>
+      <Routes>
+        <Route exact path='/' element={<div>Home</div>}/>
+      <Route path="/logs" element={<Logs/>} />
+      </Routes>
+    </div> 
+  )
 }
 
 export default App;
