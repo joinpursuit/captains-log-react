ed7145f02b5265b2a2913ed585592944c2213367
Nima sherpa <Toplouie78@gmail.com>
2022-01-13 19:33:11 -0500

    created a nav page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Logs from "./Components /logs";
+
 function App() {
-  return <div>Hello World</div>;
+	return (
+		<div>
+			<BrowserRouter>
+				<nav>
+					<h1>
+						<Link to="/logs">Captain's Log</Link>
+					</h1>
+					<button>
+						<Link to="/logs/new">New Log</Link>
+					</button>
+				</nav>
+				<Routes>
+					<Route></Route>
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Components /NavBar.js
@@ -0,0 +1,8 @@
+// export default function NavBar() {
+// 	return (
+// 		<nav>
+// 			<h1>Captain's Log</h1>
+// 			<button>New Log</button>
+// 		</nav>
+// 	);
+// }
