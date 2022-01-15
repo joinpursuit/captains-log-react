5985bdf919a3686e7c262c20776992cd885e4291
Rose Durandisse <rosedurandisse@pursuit.org>
2022-01-15 14:44:58 -0500

    Update the show page

---
+++ src/Pages/Show.js
@@ -0,0 +1,12 @@
+import LogDetails from "../Components/LogDetails";
+
+function ShowPage() {
+return (
+<div className="logDetails">
+    <h2>Log Details</h2>
+    <LogDetails />
+</div>
+);
+}
+
+export default ShowPage;
