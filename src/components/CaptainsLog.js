e033de983439074da36a378208c12708ba5db9f8
Lakisha J <lakishajohnson@pursuit.org>
2022-01-15 15:19:57 -0500

    Made log title a link to show log details

--- src/components/CaptainsLog.js
+++ src/components/CaptainsLog.js
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
 
-function CaptainsLog({ note, index }) {
+function CaptainsLog({ log, index }) {
   return (
-    <div>
-      <Link to={`/logs/${index}`}>{note.title}</Link>
-    </div>
+    <tr>
+      {/* <td>Title: {note.title}</td> */}
+      <td>
+        <Link to={`/logs/${index}`}>{log.title}</Link>
+      </td>
+    </tr>
   );
 }
 
