5c686a29893060abc875b765cd1775bbb18cdf49
Lakisha J <lakishajohnson@pursuit.org>
2022-01-15 15:22:36 -0500

    Added all my routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+// COMPONENTS
 import NavBar from "./Components/Navbar";
+
+// PAGES AKA REACT ROUTES
 import Home from "./Pages/Home";
 import Index from "./Pages/Index";
+import Show from "./Pages/Show";
+import Edit from "./Pages/Edit";
+// NEW && 404
 
 function App() {
   return (
     <div className="App">
-        {/* <h1>IT WORKS</h1> */}
+      {/* <h1>IT WORKS</h1> */}
       <Router>
         <NavBar />
-
+        <main>
           <Routes>
-          <Route path="/" element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route path="/logs" element={<Index />} />
+            <Route path="/logs/:index" element={<Show />} />
+            {/* <Route path="/bookmarks/new" element={<New />} /> */}
+            <Route path="/logs/:index/edit" element={<Edit />} />
+            {/* <Route path="*" element={<FourOFour />} /> */}
           </Routes>
-      
+        </main>
       </Router>
     </div>
   );
