68a647105e203806706e452d0a4da8d6e64b78b5
Lakisha J <lakishajohnson@pursuit.org>
2022-01-16 19:43:28 -0500

    New Log Component Added

---
+++ src/Pages/New.js
@@ -0,0 +1,12 @@
+import NewLogForm from "../Components/NewLogForm";
+
+function New() {
+  return (
+    <div className="New">
+      <h2>New</h2>
+      <NewLogForm />
+    </div>
+  );
+}
+
+export default New;
