9c8e57019f59e338b4be73f69bd5903b61837952
Rohan-Sterling <rohansterling@pursuit.org>
2022-01-17 18:25:12 -0500

    import Logs and EditLogs from components folder, add routes for single log, edit single log and add new log

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react"
 import NavBar from "./Components/Common/NavBar"
-import Logs from "./Components/Pages/Routes/Logs"
+import Logs from "./Components/Logs"
+import EditLog from "./Components/EditLog"
 import {Routes, Route} from "react-router-dom"
+import LogsDetails from "./Components/LogDetail"
 
 function App() {
   return (
@@ -9,7 +11,10 @@ function App() {
       <NavBar/>
       <Routes>
         <Route exact path='/' element={<div>Home</div>}/>
-      <Route path="/logs" element={<Logs/>} />
+        <Route path="/logs" element={<Logs/>} />
+        <Route path="/logs/:index" element={<LogsDetails/>} />
+        <Route path="/logs/:index/edit" element={<EditLog/>}/>
+        <Route path="/logs/new" element={<div>New</div>} />
       </Routes>
     </div> 
   )
