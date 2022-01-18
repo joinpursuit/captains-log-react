a38f236e7bca1b0791c301af76090cca10e67208
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2022-01-18 08:47:53 -0500

    adds edit & back button

--- src/Pages/Show.js
+++ src/Pages/Show.js
@@ -1,12 +1,24 @@
 import React from "react";
 import LogDetails from "../Components/LogDetails";
+import { Link, useParams } from "react-router-dom";
 
 function Show() {
+    let { index } = useParams();
     return (
+        <>
         <div>
             <h2>Show</h2>
             <LogDetails />
         </div>
+        <div>
+        <Link to={`/logs/${index}/edit`}>
+          <button>Edit</button>
+        </Link>
+        </div>
+        <Link to={"/logs"}>
+          <button>Back</button>
+        </Link>
+      </>
     );
 }
 
