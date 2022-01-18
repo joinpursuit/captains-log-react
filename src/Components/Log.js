e488eebb7c143ac2285c1e0d1641f79c1584659d
Rohan-Sterling <rohansterling@pursuit.org>
2022-01-17 21:05:05 -0500

    add Log helper function

---
+++ src/Components/Log.js
@@ -0,0 +1,28 @@
+const Log = ({log, index}) => {
+    return(
+        <tr className="Log">
+        <td>
+          {log.mistakesWereMadeToday ? (
+            <span>Yes</span>
+          ) : (
+            <span>No</span>
+          )}
+        </td>
+        <td>
+          <a href={`/logs/${index}`}>
+            {log.captainName} 
+
+          </a>
+
+        </td>
+        <td>
+           <a href={`/logs/${index}`}>
+              {log.title}
+          </a>
+        </td>
+      </tr>
+
+    )
+ }
+
+export default Log;
