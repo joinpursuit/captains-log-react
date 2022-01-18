a7a20a7db5e98368eaedbb4bcff4d30b34973aa3
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2022-01-18 07:47:25 -0500

    updatess import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
-import LogDetails from "./Components/LogDetails";
 
 import AllLogs from "./Pages/AllLogs"
 import Edit from "./Pages/Edit";
@@ -7,6 +6,7 @@ import New from "./Pages/New";
 import FourOFour from "./Pages/FourOFour";
 import Home from "./Pages/Home";
 import NavBar from "./Pages/NavBar";
+import Show from "./Pages/Show";
 
 function App() {
   return (
@@ -18,7 +18,7 @@ function App() {
           <Route path="/logs" element={<AllLogs />} />
           <Route path="*" element={<FourOFour />} />
           <Route path="/logs/new" element={<New/>} />
-          <Route path="/logs/:index" element={<LogDetails />} />
+          <Route path="/logs/:index" element={<Show />} />
           <Route path="/logs/:index/edit" element={<Edit />} />
         </Routes>
       </div>
