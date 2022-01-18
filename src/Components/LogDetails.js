473e888f0cdaa0bb3eb06741723ecacc31f28118
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2022-01-18 07:01:29 -0500

    updates GET request

--- src/Components/LogDetails.js
+++ src/Components/LogDetails.js
@@ -3,53 +3,55 @@ import { useState, useEffect } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import Log from "./Log";
 
-function LogDetails() {
-    const [log, setLog] = useState([]);
-    let { index } = useParams();
-    const navigate = useNavigate();
 
-    useEffect(() => {
-      async function fetchApi() {
-          const response = await axios.get(`${URL}/logs`);
-          setLog(response.data)
-      }
-      fetchApi();
+function LogDetails() {
+  const URL = process.env.REACT_APP_API_URL;
+  const [log, setLog] = useState([]);
+  let { index } = useParams();
+  const navigate = useNavigate();
+
+
+  useEffect(() => {
+    axios
+    .get(`${URL}/logs/${index}`)
+    .then((response) => setLog(response.data));
   }, []);
 
-    const handleDelete = () => {
-      axios.delete(`${URL}/logs/${index}`).then(() => navigate("/logs"));
-    };
+  const handleDelete = () => {
+    axios
+    .delete(`${URL}/logs/${index}`)
+    .then(() => navigate("/logs"));
+  };
 
-    return (
-        <article>
-              <h2>Show</h2>
-              <p></p>
-              <h3>
-                {log.mistakesWereMadeToday ? <span>⭐️</span> : null} {log.captainName}
-              </h3>
-              <h5>
-                {log.title}
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              </h5>
-              <h6>{log.post}</h6>
-              <p>Days since last crisis: {log.daysSinceLastCrisis}</p>
-              <div>
-                <div>
-                    <a href ={"/logs"}>
-                        <button>Back</button>
-                    </a>
-                </div>
-                <div>
-                    <a href={`logs/${index}/edit`}>
-                        <button>Edit</button>
-                    </a>
-                </div>
-                </div>
-                <div>
-                  {" "}
-                  <button onClick={handleDelete}>Delete</button>
-                </div>
-            </article>
+
+  return (
+    <article>
+      <h3>
+        {log.mistakesWereMadeToday ? <span>⭐️</span> : null} {log.captainName}
+      </h3>
+      <h4>
+        Title: {log.title}
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      </h4>
+      <p>Post: "{log.post}"</p>
+      <p>Days since last crisis: {log.daysSinceLastCrisis}</p>
+      <div>
+        <div>
+          <a href={"/logs"}>
+            <button>Back</button>
+          </a>
+        </div>
+        <div>
+          <a href={`/logs/${index}/edit`}>
+            <button>Edit</button>
+          </a>
+        </div>
+      </div>
+      <div>
+        {" "}
+        <button onClick={handleDelete}>Delete</button>
+      </div>
+    </article>
           );
 }
 
