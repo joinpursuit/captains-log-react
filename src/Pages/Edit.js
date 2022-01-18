99cfe2f65321842f6ec486e87c02310f31b12635
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2022-01-18 08:44:20 -0500

    adds back button

--- src/Pages/Edit.js
+++ src/Pages/Edit.js
@@ -1,12 +1,21 @@
 import React from "react";
 import LogEdit from "../Components/LogEdit";
+import { Link, useParams } from "react-router-dom";
 
 function Edit() {
+    let { index } = useParams();
     return (
-        <div>
-            <h2>Edit</h2>
-            <LogEdit />
-        </div>
+        <>
+            <div>
+                <h2>Edit</h2>
+                <LogEdit />
+            </div>
+            <div>
+                <Link to={`/logs/${index}`}>
+                    <button>Back</button>
+                </Link>
+            </div>
+        </>
     );
 }
 
