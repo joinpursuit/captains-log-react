d5cc9e619fd976e5da0f15433076a0ea58c1e403
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2022-01-18 08:43:55 -0500

    adds cancel button

--- src/Pages/New.js
+++ src/Pages/New.js
@@ -1,12 +1,20 @@
 import React from "react";
 import NewLog from "../Components/NewLog";
+import { Link } from "react-router-dom";
 
 function New() {
     return (
-        <div>
-            <h2>New Log</h2>
-            <NewLog />
-        </div>
+        <>
+            <div>
+                <h2>New Log</h2>
+                <NewLog />
+            </div>
+            <div>
+                <Link to ={"/logs"}>
+                    <button>Cancel</button>
+                </Link>
+            </div>
+        </>
     );
 }
 
