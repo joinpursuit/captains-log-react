2c947200ea7acd0fb32e160353ae49d7fea2e122
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2022-01-18 06:45:09 -0500

    edits imports

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import { Routes, Route } from "react-router-dom";
-import NavBar from "./Pages/NavBar";
-import Home from "./Pages/Home";
-import FourOFour from "./Pages/FourOFour";
-import LogsApi from "./Components/LogsApi";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import LogDetails from "./Components/LogDetails";
 
+import AllLogs from "./Pages/AllLogs"
+import Edit from "./Pages/Edit";
+import New from "./Pages/New";
+import FourOFour from "./Pages/FourOFour";
+import Home from "./Pages/Home";
+import NavBar from "./Pages/NavBar";
 
 function App() {
   return (
-    <div>
-      <NavBar />  
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/logs" element={<LogsApi />} />
-        <Route path="*" element={<FourOFour />} />
-        <Route path="/logs/:index" element={<LogDetails />} />
-      </Routes>
-    </div>
+    <Router>
+      <div>
+        <NavBar />  
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/logs" element={<AllLogs />} />
+          <Route path="*" element={<FourOFour />} />
+          <Route path="/logs/new" element={<New/>} />
+          <Route path="/logs/:index" element={<LogDetails />} />
+          <Route path="/logs/:index/edit" element={<Edit />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
