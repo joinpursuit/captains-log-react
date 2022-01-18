c12f581d06e3f918fca0dd3ed3211b510b5f6a70
Rohan-Sterling <rohansterling@pursuit.org>
2022-01-17 21:06:09 -0500

    import Link, useParams and useNavigate, add handleDelete function

--- src/Components/LogDetail.js
+++ src/Components/LogDetail.js
@@ -1,12 +1,13 @@
-import {Link, useParams} from "react-router-dom"
+import {Link, useParams, useNavigate} from "react-router-dom"
 import {useState, useEffect} from "react"
 import axios from "axios"
 
 const URL = process.env.REACT_APP_URL
-console.log(URL)
 
 const LogsDetails = () => {
     const {index} = useParams()
+
+    const navigate = useNavigate()
 
     const [log , setLog] = useState({})
 
@@ -18,15 +19,23 @@ const LogsDetails = () => {
         FetchData()
     }, [])
 
+    const handleDelete = () => {
+        axios
+        .delete(`${URL}/logs/${index}`)
+        .then(()=> navigate("/logs"))
+    }
+
     return (
         <div>
            <h1>Show</h1>
             <h2>{log.title} - By {log.captainName}</h2>
             <p>{log.post}</p>
             <p>Days since last crisis: {log.daysSinceLastCrisis}</p>
-            <Link to={`/logs`}>BACK</Link>
-            <Link to={`/logs/${index}/edit`}>EDIT</Link>
-            <Link to={`/logs`}>DELETE</Link>
+            <Link to={`/logs`}>Back</Link>
+            <Link to={`/logs/${index}/edit`}>Edit</Link>
+            <Link to={`/logs`}>
+                <button onClick={handleDelete} >Delete</button>
+            </Link>
         </div>
         
     )
