43eae4a2e08135be6886f76536cdb626982dc8d5
Laiba Sajid Khan <sajid.laiba378@gmail.com>
2022-01-19 13:50:42 -0500

    last DELETE test not passing, last EDIT test not passing

--- src/components/LogDetails.js
+++ src/components/LogDetails.js
@@ -31,16 +31,21 @@ const LogDetails = () => {
         <article className="log-card">
             <div className="card-details">  
                 <h4>{log.title} - By {log.captainName}</h4>
-                <h4>{log.post}</h4>
-                <h4>{log.mistakesWereMadeToday}</h4>
+                <p>{log.post}</p>
+                <h4>
+                    Mistakes Made today:
+                    {log.mistakesWereMadeToday ? 
+                    (<span>💥 Yes</span>) : 
+                    (<span>🙅‍♂‍ No</span>)}
+                </h4>
                 <h4>Days since last crisis: {log.daysSinceLastCrisis}</h4>
             </div>
             <div className="buttons">
                 <Link to={`/logs`}>
-                    <button> BACK </button>
+                    <button> Back </button>
                 </Link>
-                <Link to={`/logs/${index}/edit`}> <button> EDIT </button> </Link>
-                <button onClick={handleDelete}> DELETE </button>
+                <Link to={`/logs/${index}/edit`}> <button> Edit </button> </Link>
+                <button onClick={handleDelete}> Delete </button>
             </div>
         </article>
     )
