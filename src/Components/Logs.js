c72ba98240d580dd363076217bf5629765794691
salrey <tekdehn@gmail.com>
2022-01-20 20:24:25 -0500

    Add sorting, filtering options

--- src/Components/Logs.js
+++ src/Components/Logs.js
@@ -1,47 +1,72 @@
 import { useState, useEffect } from "react";
 import FormatLog from "./FormatLog.js";
-import axios from "axios"
+import axios from "axios";
 
 function Logs() {
-    const [logs, setLogs] = useState([]);
-    const URL = process.env.REACT_APP_API_URL;
+  const [logs, setLogs] = useState([]);
+  const [filter, setFilter] = useState({
+    order: "",
+    mistakesWereMadeToday:"",
+    daysSinceLastCrisis:""
+  })
+  const URL = process.env.REACT_APP_API_URL;
+  useEffect(() => {
+    axios.get(`${URL}/logs?order=${filter.order}&mistakesWereMadeToday=${filter.mistakesWereMadeToday}&daysSinceLastCrisis=${filter.daysSinceLastCrisis}`)
+    .then((response) => {console.log(response.data); return setLogs(response.data)});
+  }, [URL, filter.order, filter.mistakesWereMadeToday, filter.daysSinceLastCrisis]);
 
-    useEffect(() => {
-      axios.get(URL+"/logs")
-        .then((response) => setLogs(response.data))
-    }, [URL])
-  
-    // const handleSort = () => {
+  const handleChange = (event) => {
+    filter[event.target.name] !== event.target.value && setFilter({...filter, [event.target.name]: event.target.value})
+  };
 
-    // }
-
-    return (
-      <div className="Logs container p-5 my-5 bg-warning">
-        <section>
-          <table>
-            <thead>
-            </thead>
-            <tbody className="list-group">
-              {logs.map((log, index) => {
-                return <FormatLog key={index} log={log} index={index} />;
-              })}
-            </tbody>
-          </table>
-        </section>
-        <button 
-          className="btn btn-outline-primary mt-3"
-          data-bs-toggle="collapse" 
+  return (
+    <div className="Logs container p-5 my-5 bg-warning">
+        <button
+          className="btn btn-outline-primary mb-3"
+          data-bs-toggle="collapse"
           data-bs-target="#demo">
-            Sort
+          Filter
         </button>
-        <div id="demo" className="container-fluid collapse form-check text-primary text-center">
-            <input className="p-2" type="radio" id="title" name="sortBy"/>
-            <label className="p-2">Title</label>
-            <input className="p-2" type="radio" id="name" name="sortBy"/>
-            <label className="p-2">Name</label>
+      <form id="form" className="container mb-4" action="/logs" method="GET">
+        <div
+          id="demo"
+          className="row collapse justify-content-center form-group text-primary text-center text-muted small">
+            <label className="col-3" htmlFor="order">Sort by
+              <select onChange={handleChange} className="form-control-sm bg-warning text-muted m-2" name="order">
+                <option className="dropdown-item" value="">---</option>
+                <option className="dropdown-item" value="asc">{`Name (Asc)`}</option>
+                <option className="dropdown-item" value="desc">{`Name (Desc)`}</option>
+              </select>
+            </label>
+            <label className="col-4" htmlFor="daysSinceLastCrisis">Days Since Last Crisis?
+              <select onChange={handleChange} className="form-control-sm bg-warning text-muted m-2" name="daysSinceLastCrisis">
+                <option className="dropdown-item" value="">---</option>
+                <option className="dropdown-item" value="lte5">at most 5</option>
+                <option className="dropdown-item" value="gt10">more than 10</option>
+                <option className="dropdown-item" value="gte20">at least 20</option>
+              </select>
+            </label>
+            <label className="col-4" htmlFor="mistakesWereMadeToday">Any Mistakes?
+              <select onChange={handleChange} className="form-control-sm bg-warning text-muted m-2" name="mistakesWereMadeToday">
+                <option className="dropdown-item" value="">---</option>
+                <option className="dropdown-item" value="true">Yes</option>
+                <option className="dropdown-item" value="false">No</option>
+              </select>
+            </label>
         </div>
-      </div>
-    );
-  }
-  
-  export default Logs;
+      </form>
+      <section>
+        <table>
+          <thead></thead>
+          <tbody className="list-group">
+            {logs.map((log, index) => {
+              return <FormatLog key={index} log={log} index={index} />;
+            })}
+          </tbody>
+        </table>
+      </section>
+    </div>
+  );
+}
+
+export default Logs;
