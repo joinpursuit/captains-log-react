ca431a1ecd26e0c29f7e48868e11ae033756b3d6
Jonathan Scheiber <jon@scheiber.me>
2022-07-19 16:07:23 -0400

    create routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./Components/NavBar";
+import Edit from "./Components/Edit";
+import Home from "./Components/Home";
+import Index from "./Components/Index";
+import New from "./Components/New";
+import Show from "./Components/Show";
 
 function App() {
   return (
@@ -8,6 +13,11 @@ function App() {
         <NavBar />
         <main>
           <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/logs" element={<Index />} />
+            <Route path="/logs/new" element={<New />} />
+            <Route exact path="/logs/:index" element={<Show />} />
+            <Route path="/logs/:index/edit" element={<Edit />} />
           </Routes>
         </main>
       </Router>
