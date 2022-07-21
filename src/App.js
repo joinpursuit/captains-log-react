55d8cea1dcc3c4fd39798ff77601b0f8eda4d0d3
Derek <plutus.derekpeterson@gmail.com>
2022-07-21 09:37:47 -0400

    Lab Done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+//DEPENDENCIES
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+//COMPONENTS
+import NavBar from './Components/NavBar';
+
+//PAGES
+import Edit from './Pages/Edit';
+import ErrorPage from './Pages/ErrorPage';
+import Home from './Pages/Home';
+import Index from './Pages/Index';
+import New from './Pages/New';
+import Show from './Pages/Show';
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div className='App'>
+      <Router>
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/logs' element={<Index />} />
+            <Route path='/logs/new' element={<New />} />
+            <Route path='/logs/:index' element={<Show />} />
+            <Route path='/logs/:index/edit' element={<Edit />} />
+            <Route path='*' element={<ErrorPage />} />
+          </Routes>
+        </main>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Pages/Show.js
@@ -0,0 +1,12 @@
+import LogDetails from '../Components/LogDetails.js';
+
+function Show() {
+  return (
+    <div>
+      <h1>Show</h1>
+      <LogDetails />
+    </div>
+  );
+}
+
+export default Show;
