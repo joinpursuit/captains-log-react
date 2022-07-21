f09e0643f8b4b0e5aeb672bd001f68586fe62421
Abdullah-Tasfiek <atasfiek.ece@gmail.com>
2022-07-21 13:44:01 -0400

    passed all tests except Karolin test in edit.cy.js

---
+++ src/Components/LogDetails.js
@@ -0,0 +1,65 @@
+import { useState, useEffect } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+const API = process.env.REACT_APP_API_URL;
+
+function LogDetails() {
+  const [log, setLog] = useState([]);
+  let { index } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get(`${API}/logs/${index}`)
+      .then((res) => {
+        setLog(res.data);
+      })
+      .catch(() => {
+        navigate("/not-found");
+      });
+  }, [index, navigate]);
+
+  const handleDelete = () => {
+    axios
+      .delete(`${API}/logs/${index}`)
+      .then(() => {
+        navigate("/logs");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <article>
+      <h3>
+        {log.title} - By {log.captainName}
+      </h3>
+      <p>{log.post}</p>
+      <p>
+        <strong>Days since last crisis: </strong>
+        {log.daysSinceLastCrisis}
+      </p>
+      <div className="showNavigation">
+        <div>
+          {" "}
+          <Link to={`/logs`}>
+            <button>Back</button>
+          </Link>
+        </div>
+        <div>
+          {" "}
+          <Link to={`/logs/${index}/edit`}>
+            <button>Edit</button>
+          </Link>
+        </div>
+        <div>
+          {" "}
+          <button onClick={handleDelete}>Delete</button>
+        </div>
+      </div>
+    </article>
+  );
+}
+
+export default LogDetails;
---
+++ src/Pages/Edit.js
@@ -0,0 +1,11 @@
+import LogEditForm from "../Components/LogEditForm";
+import React from "react";
+
+export default function Edit() {
+  return (
+    <div>
+      <h2>Edit</h2>
+      <LogEditForm />
+    </div>
+  );
+}
