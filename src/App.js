dbad4d2560cd4b25ebf2ab3208e813870b7170a6
PratimaRoy <pratimaroy170@gmail.com>
2022-07-20 20:37:27 -0400

    Learned to connect frontend to backend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import NavBar from "./Components/NavBar";
+
+import Home from "./Pages/Home";
+import Edit from "./Pages/Edit";
+import Index from "./Pages/Index";
+import New from "./Pages/New";
+import Show from "./Pages/Show";
+import FourOFour from "./Pages/FourOFour";
+
+
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div className="App">
+      <Router>
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/logs" element={<Index />} />
+            <Route path="/logs/new" element={<New />} />
+            <Route path="/logs/:index" element={<Show />} />
+            <Route path="/logs/:index/edit" element={<Edit />} />
+            <Route path="*" element={<FourOFour />} />
+          </Routes>
+        </main>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
