032d0db6616e89fe400f910887b173b970c8c158
Jonathan Scheiber <jon@scheiber.me>
2022-07-21 03:32:36 -0400

    update pages

---
+++ src/Pages/Show.js
@@ -0,0 +1,12 @@
+import LogDetails from "../Components/LogDetails";
+
+function Show() {
+  return (
+    <div className="Show">
+      <h2>Show Captain's Log</h2>
+      <LogDetails />
+    </div>
+  );
+}
+
+export default Show;
